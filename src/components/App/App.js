166f61451115e8a19cff71fe8d540a142a3156b4
Jemel Preval <cabinfeverchronicles@gmail.com>
2020-04-20 18:19:55 -0700

    removed classname from line 120

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -117,7 +117,7 @@ export default class App extends React.Component {
     if (this.state.finishedLoading && this.state.isVisible) {
       return (
         <div className="circleContainer" style={{zIndex: '1', borderRadius:'50%', width: '100px', height: '100px', background: 'lightblue', paddingBottom:'15px'}}>
-          <h1 style={{ textAlign: 'center', fontSize: '100px', color: "white", marginBottom:'2px' }}>{comboCount, className='comboCountBase'}</h1>
+          <h1 style={{ textAlign: 'center', fontSize: '100px', color: "white", marginBottom:'2px' }}>{comboCount}</h1>
         </div>
       )
     } else {
